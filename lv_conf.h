260e4a48ca258fb9fa98609fd9a24bb0f148aea4
Amir Mobasheraghdam <142031484+Amirmobash@users.noreply.github.com>
2026-01-28 07:07:51 -1100

    Update lv_conf.h

--- lv_conf.h
+++ lv_conf.h
@@ -1,22 +1,13 @@
 #ifndef LV_CONF_H
 #define LV_CONF_H
 
-/* =====================
- *  Grund-Konfiguration
- * ===================== */
 #define LV_COLOR_DEPTH 16
-/* Für RGB Panels meistens 0 richtig. Wenn Farben vertauscht: 1 */
 #define LV_COLOR_16_SWAP 0
 
-/* Speicher für LVGL (bei PSRAM darf höher sein) */
 #define LV_MEM_SIZE (128U * 1024U)
 
-/* Logging (optional) */
 #define LV_USE_LOG 0
 
-/* =====================
- *  Fonts (UI groß, Industrie)
- * ===================== */
 #define LV_FONT_MONTSERRAT_14 1
 #define LV_FONT_MONTSERRAT_16 1
 #define LV_FONT_MONTSERRAT_18 1
@@ -25,9 +16,6 @@
 #define LV_FONT_MONTSERRAT_24 1
 #define LV_FONT_MONTSERRAT_28 1
 
-/* =====================
- *  Widgets (Standard)
- * ===================== */
 #define LV_USE_LABEL 1
 #define LV_USE_BTN 1
 #define LV_USE_BAR 1
@@ -35,4 +23,4 @@
 #define LV_USE_TEXTAREA 1
 #define LV_USE_KEYBOARD 1
 
-#endif /*LV_CONF_H*/
+#endif
