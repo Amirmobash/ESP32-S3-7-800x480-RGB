5e4933af0f21c1c85effefb5aad5bf0a94433637
Amir Mobasheraghdam <142031484+Amirmobash@users.noreply.github.com>
2026-02-05 07:17:41 -1100

    Update lv_conf.h

--- lv_conf.h
+++ lv_conf.h
@@ -1,46 +1,72 @@
+/* lv_conf.h - Minimal config for LVGL 8.x (ESP32-S3 RGB 800x480) */
 #ifndef LV_CONF_H
 #define LV_CONF_H
 
-/* =====================
- *  LVGL v8.x CONFIG
- * ===================== */
+/*====================
+   COLOR SETTINGS
+ *====================*/
+#define LV_COLOR_DEPTH              16
+#define LV_COLOR_16_SWAP            0
 
-#define LV_COLOR_DEPTH 16
-#define LV_COLOR_16_SWAP 0
+/*====================
+   MEMORY SETTINGS
+ *====================*/
+#define LV_MEM_CUSTOM               0
+#define LV_MEM_SIZE                 (128U * 1024U)
 
-#define LV_MEM_CUSTOM 0
-#define LV_MEM_SIZE (64U * 1024U)
+/*====================
+   HAL SETTINGS
+ *====================*/
+#define LV_TICK_CUSTOM              0
 
-#define LV_USE_LOG 0
+/*====================
+   FEATURE CONFIG
+ *====================*/
+#define LV_USE_LOG                  0
+#define LV_USE_PERF_MONITOR         0
 
-#define LV_TICK_CUSTOM 1
-#define LV_TICK_CUSTOM_INCLUDE "Arduino.h"
-#define LV_TICK_CUSTOM_SYS_TIME_EXPR ((uint32_t)millis())
+/*====================
+   FONT USAGE
+ *====================*/
+#define LV_FONT_MONTSERRAT_14       1
+#define LV_FONT_MONTSERRAT_16       1
+#define LV_FONT_MONTSERRAT_18       1
+#define LV_FONT_MONTSERRAT_20       1
+#define LV_FONT_MONTSERRAT_22       1
+#define LV_FONT_MONTSERRAT_24       1
+#define LV_FONT_MONTSERRAT_28       1
+#define LV_FONT_MONTSERRAT_36       1
+#define LV_FONT_MONTSERRAT_48       1
+#define LV_FONT_DEFAULT             &lv_font_montserrat_20
 
-#define LV_DPI_DEF 130
+/*====================
+   TEXT SETTINGS
+ *====================*/
+#define LV_TXT_ENC                  LV_TXT_ENC_UTF8
+#define LV_USE_BIDI                 0
+#define LV_USE_ARABIC_PERSIAN_CHARS 0
 
-/* Draw buffers */
-#define LV_DISP_DEF_REFR_PERIOD 30
+/*====================
+   WIDGETS
+ *====================*/
+#define LV_USE_BTN                  1
+#define LV_USE_LABEL                1
+#define LV_USE_BAR                  1
+#define LV_USE_SLIDER               1
+#define LV_USE_BTNMATRIX            1
+#define LV_USE_MSGBOX               1
 
-/* Fonts */
-#define LV_FONT_MONTSERRAT_14 1
-#define LV_FONT_MONTSERRAT_16 1
-#define LV_FONT_MONTSERRAT_20 1
-#define LV_FONT_MONTSERRAT_28 1
-#define LV_FONT_DEFAULT &lv_font_montserrat_20
+/*====================
+   LAYOUTS
+ *====================*/
+#define LV_USE_FLEX                 1
+#define LV_USE_GRID                 1
 
-/* Widgets (حداقل‌های لازم) */
-#define LV_USE_LABEL 1
-#define LV_USE_BTN 1
-#define LV_USE_BAR 1
-#define LV_USE_SLIDER 1
-#define LV_USE_TEXTAREA 1
-#define LV_USE_KEYBOARD 1
-
-/* Themes */
-#define LV_USE_THEME_DEFAULT 1
-
-/* Animations */
-#define LV_USE_ANIMIMG 0
+/*====================
+   OTHERS
+ *====================*/
+#define LV_USE_ANIMATION            1
+#define LV_USE_IMG                  1
+#define LV_USE_THEME_DEFAULT        1
 
 #endif /*LV_CONF_H*/
