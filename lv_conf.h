31a959d392f20915b1ab2caab6d786fcfce218fe
Amir Mobasheraghdam <142031484+Amirmobash@users.noreply.github.com>
2026-02-05 07:24:59 -1100

    Update lv_conf.h

--- lv_conf.h
+++ lv_conf.h
@@ -2,6 +2,8 @@
 #ifndef LV_CONF_H
 #define LV_CONF_H
 
+#include <stdint.h>
+
 /*====================
    COLOR SETTINGS
  *====================*/
@@ -11,19 +13,28 @@
 /*====================
    MEMORY SETTINGS
  *====================*/
-#define LV_MEM_CUSTOM               0
+#define LV_MEM_CUSTOM               1
 #define LV_MEM_SIZE                 (128U * 1024U)
+#define LV_MEM_ATTR
 
 /*====================
    HAL SETTINGS
  *====================*/
-#define LV_TICK_CUSTOM              0
+#define LV_TICK_CUSTOM              1
+#define LV_TICK_CUSTOM_INCLUDE      "Arduino.h"
+#define LV_TICK_CUSTOM_SYS_TIME_EXPR (millis())
 
 /*====================
    FEATURE CONFIG
  *====================*/
 #define LV_USE_LOG                  0
 #define LV_USE_PERF_MONITOR         0
+#define LV_USE_ASSERT_NULL          0
+#define LV_USE_ASSERT_MALLOC        0
+#define LV_USE_ASSERT_STYLE         0
+#define LV_USE_ASSERT_OBJ           0
+#define LV_USE_ASSERT_SCREEN        0
+#define LV_USE_USER_DATA            0
 
 /*====================
    FONT USAGE
@@ -49,12 +60,22 @@
 /*====================
    WIDGETS
  *====================*/
-#define LV_USE_BTN                  1
-#define LV_USE_LABEL                1
+#define LV_USE_ARC                  1
 #define LV_USE_BAR                  1
-#define LV_USE_SLIDER               1
+#define LV_USE_BTN                  1
 #define LV_USE_BTNMATRIX            1
-#define LV_USE_MSGBOX               1
+#define LV_USE_CALENDAR             1
+#define LV_USE_CANVAS               1
+#define LV_USE_CHECKBOX             1
+#define LV_USE_DROPDOWN             1
+#define LV_USE_IMG                  1
+#define LV_USE_LABEL                1
+#define LV_USE_LINE                 1
+#define LV_USE_ROLLER               1
+#define LV_USE_SLIDER               1
+#define LV_USE_SWITCH               1
+#define LV_USE_TEXTAREA             1
+#define LV_USE_TABLE                1
 
 /*====================
    LAYOUTS
@@ -66,7 +87,13 @@
    OTHERS
  *====================*/
 #define LV_USE_ANIMATION            1
-#define LV_USE_IMG                  1
+#define LV_USE_SHADOW               1
+#define LV_USE_OBJ_POS              1
+#define LV_USE_GROUP                1
+#define LV_USE_IMG_TRANSFORM        1
+#define LV_USE_FILE_EXPLORER        0
 #define LV_USE_THEME_DEFAULT        1
+#define LV_USE_THEME_BASIC          1
+#define LV_USE_THEME_MONO           1
 
 #endif /*LV_CONF_H*/
